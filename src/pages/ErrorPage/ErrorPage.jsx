37d37547aaa5a12e1e5013cd3923688abe984601
amandeepp26 <amandeepp26@gmail.com>
2023-12-16 21:06:53 +0530

    Issue fixes

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
-import error404 from "../../assets/img/404.png"
+import error404 from "../../assets/img/notfound.png";
 
 const ErrorPage = () => {
-    return (
-        <div className="bg-[#FBFAF9]">
-            <div className="max-w-3xl mx-auto px-7 h-screen flex justify-center items-center">
-                <div>
-                    <div>
-                        <img src={error404} alt="404 image" />
-                    </div>
-                    <div className="mt-10 text-center">
-                        <Link className="text-primary-normal border-b border-primary-normal text-lg" to={'/'}> <button> Go to Home Page </button> </Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="bg-[#FBFAF9]">
+      <div className="max-w-3xl mx-auto px-7 h-screen flex justify-center items-center">
+        <div>
+          <div>
+            <img src={error404} alt="404 image" />
+          </div>
+          <div className="mt-10 text-center">
+            <Link
+              className="text-primary-normal border-b border-primary-normal text-lg"
+              to={"/"}
+            >
+              {" "}
+              <button> Go to Home Page </button>{" "}
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
-
