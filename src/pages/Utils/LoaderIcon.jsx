887da658fe216e90a76ed7d2a162f54dddc3d982
amandeepp26 <amandeepp26@gmail.com>
2023-12-16 21:13:25 +0530

    Loader icon  issues fix

--- src/pages/Utils/LoaderIcon.jsx
+++ src/pages/Utils/LoaderIcon.jsx
@@ -1,10 +1,10 @@
-import loaderIcon from "../../assets/icons/loader.gif";
+import loaderIcon from "../../assets/icons/loaderIcon.gif";
 
 const LoaderIcon = () => {
-    return (
-        <>
-            <img className="w-6" src={loaderIcon} alt="loader" />
-        </>
-    );
+  return (
+    <>
+      <img className="w-6" src={loaderIcon} alt="loader" />
+    </>
+  );
 };
 export default LoaderIcon;
