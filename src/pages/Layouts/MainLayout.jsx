f9e5a6e82cf3e383a684f3f64b43376ce8fea369
amandeepp26 <amandeepp26@gmail.com>
2023-12-16 21:05:17 +0530

    Issue fix

--- src/pages/Layouts/MainLayout.jsx
+++ src/pages/Layouts/MainLayout.jsx
@@ -1,18 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
-import bg_img from "../../assets/img/bg_img.png"
+import bg_img from "../../assets/img/bgImg.png";
 
 const MainLayout = () => {
-
-
-    return (
-        <div style={{ backgroundImage: `url(${bg_img})`}} className="bg-repeat-y bg-contain">
-            <Header/>
-            <Outlet/>
-            <Footer/> 
-        </div>
-    );
+  return (
+    <div
+      style={{ backgroundImage: `url(${bg_img})` }}
+      className="bg-repeat-y bg-contain"
+    >
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
