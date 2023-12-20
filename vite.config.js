a41c6e5737f8bd691b36992e6c1ea5c470062769
kartik126 <lavi9921@gmail.com>
2023-12-20 13:29:54 +0530

    update:package.json

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default {
   build: {
-    chunkSizeWarningLimit: 10000, // Set your preferred limit in kB
-  }
-})
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+};
+
