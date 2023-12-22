61b80dc80023b1d70328c7676e4aa75cd98a6c73
amandeepp26 <amandeepp26@gmail.com>
2023-12-22 16:27:18 +0530

    social icons link update

--- src/pages/Utils/SocialWidget.jsx
+++ src/pages/Utils/SocialWidget.jsx
@@ -1,20 +1,30 @@
-
 import { FaFacebookF, FaInstagram, FaGithub } from "react-icons/fa";
 
 const SocialWidget = () => {
-    return (
-        <div className="flex gap-5 items-center py-5">
-            <a href="https://facebook.com/sohel5g/" target="blank" className="border-2 border-primary-normal hover:border-primary-hover hover:bg-primary-hover hover:text-white p-[6px] text-primary-normal rounded-full">
-                <FaFacebookF />
-            </a>
-            <a href="#" className="border-2 border-primary-normal hover:border-primary-hover hover:bg-primary-hover hover:text-white p-[6px] text-primary-normal rounded-full">
-                <FaInstagram />
-            </a>
-            <a href="https://github.com/sohel5G" target="blank" className="border-2 border-primary-normal hover:border-primary-hover hover:bg-primary-hover hover:text-white p-[6px] text-primary-normal rounded-full">
-                <FaGithub />
-            </a>
-        </div>
-    );
+  return (
+    <div className="flex gap-5 items-center py-5">
+      <a
+        href="#"
+        target="blank"
+        className="border-2 border-primary-normal hover:border-primary-hover hover:bg-primary-hover hover:text-white p-[6px] text-primary-normal rounded-full"
+      >
+        <FaFacebookF />
+      </a>
+      <a
+        href="#"
+        className="border-2 border-primary-normal hover:border-primary-hover hover:bg-primary-hover hover:text-white p-[6px] text-primary-normal rounded-full"
+      >
+        <FaInstagram />
+      </a>
+      <a
+        href="#"
+        target="blank"
+        className="border-2 border-primary-normal hover:border-primary-hover hover:bg-primary-hover hover:text-white p-[6px] text-primary-normal rounded-full"
+      >
+        <FaGithub />
+      </a>
+    </div>
+  );
 };
 
 export default SocialWidget;
