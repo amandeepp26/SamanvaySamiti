0ad48e05730a32e996dead771558a32b54902287
amandeepp26 <amandeepp26@gmail.com>
2024-01-03 15:53:44 +0530

    Login form label update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -135,13 +135,13 @@ const Login = () => {
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900"
                 >
-                  Your email
+                  Your email or phone
                 </label>
                 <input
                   name="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                  placeholder="name@company.com"
+                  placeholder="Enter email or phone"
                   required=""
                 />
               </div>
